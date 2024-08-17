367228a5e8a2472946dd5291c3466f62e7efe8de
PrajwalMokta <prajwalmoktan296@gmail.com>
2024-08-17 13:49:25 +0545

    delete button is added in the e-library

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -96,4 +96,15 @@ class AdminController extends Controller
         $data->save();
         return redirect()->back();
     }
+    public function show_book()
+    {
+        $book = Book::all();
+        return view('admin.show_book',compact('book'));
+    }
+    public function book_delete($id)
+    {
+        $data = Book::find($id);
+        $data->delete();
+        return redirect()->back()->with('message', 'Book Deleted Successfully');
+    }
 }
